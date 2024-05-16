229f25bc59de503d7e4922fe447935446cde0a55
Fotsing2023 <ernest.fotsing@umifr.ch>
2024-05-16 18:10:27 +0200

    addexecice

---
+++ Exercice1.R
@@ -0,0 +1,20 @@
+#Assignment #1: Basic Linear Regression: Elevation and Temperature in the USA
+ExerciseA Instructions
+In a new and appropriately-named
+script file, follow the linear regression
+workflow demonstrated to analyze the
+climate data, this time for only stations in the United States. The ultimate objective is to determine how
+well elevation predicts annual temperature in this region, and to use the regression model to predict
+annual temperature for Front Royal, VA
+Proceed through the entire analysis workflow and answer the following questions. This assignment is
+worth 5 points. The readability and organization of your code will count for 0.5 points of this total
+
+Study Questions
+1. What is the regression (beta) coefficient for elevation? (0.5 pts)
+2. What is the predicted change in annual temperature, with a 100 m increase in elevation? (1 pt)
+3. How much variation in temperature is explained by elevation in the US? (1 pt)
+4. Using ggplot, generate a scatterplot of temperature vs. elevation data. Include your fitted regression
+line and give appropriate labels for both axes. (1 pt)
+5. Use your regression equation to predict the annual average temperature for Front Royal, VA, which is
+at an elevation of 170m. (1 pt)
+
